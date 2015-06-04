5616b8deca0bd47cc0467157f8980190b83d9396
gonzalosv <gsalazar1-es@udabol.edu.bo>
2015-06-04 11:35:25 -0400

    refactor code

---
+++ src/python/main.py
@@ -0,0 +1,28 @@
+from utils.simplePerceptron import find_net_i
+from utils.simplePerceptron import find_error
+from utils.simplePerceptron import re_calculate_weight
+
+vx = [[1, 0, 0],
+      [1, 0, 1],
+      [1, 1, 0],
+      [1, 1, 1]]
+
+vy = [0, 1, 1, 1]
+vw = [1, 0, 0.5]
+learn = 0.6
+
+while True:
+    print("-" * 60)
+    count = 0
+    for i in range(len(vx)):  # tour the inputs
+        print("vw", vw)
+        net_i = find_net_i(vw, vx[i])
+        error = find_error(vy[i], net_i)
+
+        if error != 0:
+            count += 1
+            vw = re_calculate_weight(vw, learn, error, vx[i])
+    if count == 0:
+        break
+
+
---
+++ src/python/utils/simplePerceptron.py
@@ -0,0 +1,45 @@
+import random
+import logging
+
+log = logging
+
+
+def get_random_weight(iteration):
+    v = []
+    for i in range(iteration):
+        v.append(random.uniform(-2, 2))
+    return v
+
+
+def get_random_factor_learning():
+    return random.uniform(0.2, 0.9)
+
+
+def get_product_vectors(v1, v2):
+    result = []
+    for i in range(len(v2)):
+        result.append(v1[i] * v2[i])
+    return result
+
+
+def get_sum_product_vectors(v1, v2):
+    if not v1 or not v2:
+        log.warning("vector is empty")
+    return sum(get_product_vectors(v1, v2))
+
+
+def re_calculate_weight(vw, learning, error, vx):
+    new_w = []
+    for i in range(len(vw)):
+        new_w.append(vw[i] + learning * error * vx[i])
+    return new_w
+
+
+def find_error(y, net_i):
+    return y - net_i
+
+
+def find_net_i(vw, vx):
+    return 1 if get_sum_product_vectors(vw, vx) > 0 else 0
+
+
